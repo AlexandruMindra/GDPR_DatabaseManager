a6c0735e24f62580a69cdd05b7a5cd732f7e77d3
Alexandru Mandra <id.meu40@yahoo.com>
2018-11-02 21:36:09 +0200

    Startup

---
+++ GDPR_DatabaseManager/GDPR_DatabaseManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDPR_DatabaseManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
